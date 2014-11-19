e42f49069564c785cbb65c70fadf805b8bdc01c6
hchsueh <hchsuehgm@gmail.com>
2014-11-19 14:48:36 +0800

    added ingr pages

--- javascripts/search.js
+++ javascripts/search.js
@@ -1,4 +1,13 @@
-document.getElementById('search_btn').onclick = function()
-{
-    console.log('search btn clicked!');
-};
+(function($) {
+
+	/*document.getElementById('search_btn').onclick = function()
+	{
+	    console.log('search btn clicked!');
+	};*/
+
+	$('#search_btn').click(function(){
+		console.log('search btn clicked!');
+	});
+
+})(jQuery);
+
